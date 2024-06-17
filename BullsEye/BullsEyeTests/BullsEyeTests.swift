a61a90346394ec69f2271fcf69f8f3e57ea3e62f
Sahil Naik <nsahil992@gmail.com>
2024-06-17 22:36:03 +0530

    feat: Modified: Scores functionality added

--- BullsEye/BullsEyeTests/BullsEyeTests.swift
+++ BullsEye/BullsEyeTests/BullsEyeTests.swift
@@ -13,13 +13,25 @@ final class BullsEyeTests: XCTestCase {
     override func setUpWithError() throws {
         game = Game()
     }
-
+    
     override func tearDownWithError() throws {
-       game = nil
+        game = nil
     }
-
+    
     func testExample() throws {
         XCTAssertEqual(game.points(sliderValue: 50), 999)
     }
-
+    
+    func testScorePositive() {
+        var guess = game.target + 5
+        var score = game.points(sliderValue: guess)
+        XCTAssertEqual(score, 95)
+        
+        func testScoreNegative() {
+            var guess = game.target - 5
+            var score = game.points(sliderValue: guess)
+            XCTAssertEqual(score, 95)
+        }
+        
+    }
 }
