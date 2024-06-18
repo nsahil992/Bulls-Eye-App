7d58623d22869a38b38c66e19ed33fb5565d2290
Sahil Naik <nsahil992@gmail.com>
2024-06-18 14:07:15 +0530

    feat: Modified: Assets updated

--- BullsEye/BullsEye/Views/ContentView.swift
+++ BullsEye/BullsEye/Views/ContentView.swift
@@ -12,56 +12,60 @@ struct ContentView: View {
     @State private var sliderValue = 50.0
     @State private var game = Game()
     var body: some View {
-        VStack {
-            Text("🎯🎯🎯\nPut the bull's eye as close as you can".uppercased())
-                .bold()
-                .multilineTextAlignment(.center)
-                .lineSpacing(4.0)
-                .font(.footnote)
-                .kerning(2.0)
-                .padding(.horizontal, 30)
-            Text(String(game.target))
-                .kerning(-1.0)
-                .font(.largeTitle)
-                .fontWeight(.black)
-            HStack {
-                Text("1")
+        ZStack {
+            Color("BackgroundColor")
+                .ignoresSafeArea()
+            VStack {
+                Text("🎯🎯🎯\nPut the bull's eye as close as you can".uppercased())
                     .bold()
-                Slider(value: $sliderValue, in: 1.0...100.0)
-                Text("100")
-                    .bold()
-            }
-            .padding()
-            Button("Hit me".uppercased()) {
-                alertIsVisible = true
-            }
-            .padding(20.0)
-            .background(
-                Color.blue
-            )
-            .foregroundColor(.white)
-            .cornerRadius(21.0)
-            .bold()
-            .font(.title3)
-            .alert("Button Pressed",
-                   isPresented: $alertIsVisible,
-                   actions: {
-                Button("Cancel"){
-                    
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(4.0)
+                    .font(.footnote)
+                    .kerning(2.0)
+                    .padding(.horizontal, 30)
+                Text(String(game.target))
+                    .kerning(-1.0)
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                HStack {
+                    Text("1")
+                        .bold()
+                    Slider(value: $sliderValue, in: 1.0...100.0)
+                    Text("100")
+                        .bold()
                 }
-            },
-                   message: {
-                let roundedValue = Int(sliderValue)
-                Text("""
+                .padding()
+                Button("Hit me".uppercased()) {
+                    alertIsVisible = true
+                }
+                .padding(20.0)
+                .background(
+                    Color("ButtonColor")
+                )
+                .foregroundColor(.white)
+                .cornerRadius(21.0)
+                .bold()
+                .font(.title3)
+                .alert("Button Pressed",
+                       isPresented: $alertIsVisible,
+                       actions: {
+                    Button("Cancel"){
+                        
+                    }
+                },
+                       message: {
+                    let roundedValue = Int(sliderValue)
+                    Text("""
                      The slider's value is \(roundedValue).
                      You scored \(game.points(sliderValue: roundedValue)) in this round.
                      """)
-            })
+                })
+            }
         }
     }
-}
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
