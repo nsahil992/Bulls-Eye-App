4cf1434bebbea01175436a742175233a8e062212
Sahil Naik <nsahil992@gmail.com>
2024-06-20 14:07:43 +0530

    feat: Modified: Code made clean and easy to read

--- BullsEye/BullsEye/Views/ContentView.swift
+++ BullsEye/BullsEye/Views/ContentView.swift
@@ -19,49 +19,12 @@ struct ContentView: View {
                 .ignoresSafeArea()
             VStack {
                 InstructionView(game: $game)
-                HStack {
-                    Text("1")
-                        .bold()
-                        .foregroundColor(Color("TextColor"))
-                    Slider(value: $sliderValue, in: 1.0...100.0)
-                    Text("100")
-                        .bold()
-                        .foregroundColor(Color("TextColor"))
-                }
-                .padding()
-                Button("Hit me".uppercased()) {
-                    alertIsVisible = true
-                }
-                .padding(20.0)
-                .background(
-                    ZStack {
-                        Color("ButtonColor")
-                        LinearGradient(
-                            gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]),
-                            startPoint: .top, endPoint: .bottom)
-                    }
-                )
-                .foregroundColor(.white)
-                .cornerRadius(21.0)
-                .bold()
-                .font(.title3)
-                .alert("Button Pressed",
-                       isPresented: $alertIsVisible,
-                       actions: {
-                    Button("Cancel"){
-                        
-                    }
-                },
-                       message: {
-                    let roundedValue = Int(sliderValue)
-                    Text("""
-                     The slider's value is \(roundedValue).
-                     You scored \(game.points(sliderValue: roundedValue)) in this round.
-                     """)
-                })
+                SliderView(sliderValue: $sliderValue)
+                HitMeButton(alertIsVisible: $alertIsVisible, sliderValue: $sliderValue, game: $game)
             }
         }
     }
+    
     struct InstructionView: View {
         @Binding var game: Game
         var body: some View {
@@ -74,9 +37,67 @@ struct ContentView: View {
     }
     
     
+    struct SliderView: View {
+        @Binding var sliderValue: Double
+        
+        var body: some View {
+            HStack {
+                SliderLabelText(text: "1")
+                Slider(value: $sliderValue, in: 1.0...100.0)
+                SliderLabelText(text: "100")
+            }
+            .padding()
+        }
+    }
+    
+    
+    
+    struct HitMeButton: View {
+        @Binding var alertIsVisible: Bool
+        @Binding var sliderValue: Double
+        @Binding var game: Game
+        
+        var body: some View {
+            Button("Hit me".uppercased()) {
+                alertIsVisible = true
+            }
+            .padding(20.0)
+            .background(
+                ZStack {
+                    Color("ButtonColor")
+                    LinearGradient(
+                        gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]),
+                        startPoint: .top, endPoint: .bottom)
+                }
+            )
+            .foregroundColor(.white)
+            .cornerRadius(21.0)
+            .bold()
+            .font(.title3)
+            .alert("Button Pressed",
+                   isPresented: $alertIsVisible,
+                   actions: {
+                Button("Cancel"){
+                    
+                }
+            },
+                   message: {
+                let roundedValue = Int(sliderValue)
+                Text("""
+                 The slider's value is \(roundedValue).
+                 You scored \(game.points(sliderValue: roundedValue)) in this round.
+                 """)
+            })
+        }
+    }
+    
+    
     struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
             ContentView()
+            ContentView()
+                .preferredColorScheme(.dark)
+                .previewDevice("iPhone 14 Pro Max")
         }
     }
 }
