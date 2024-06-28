097fdfb103daff147e641983cffc88cb9dd66616
Sahil Naik <nsahil992@gmail.com>
2024-06-28 22:45:31 +0530

    feat: Debugger

--- BullsEye/BullsEye/Views/ContentView.swift
+++ BullsEye/BullsEye/Views/ContentView.swift
@@ -62,6 +62,7 @@ struct ContentView: View {
         var body: some View {
             Button("Hit me".uppercased()) {
                 alertIsVisible = true
+                game.startNewRound(points: game.points(sliderValue: Int(sliderValue)))
             }
             .padding(20.0)
             .background(
